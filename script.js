ef232fdfb4a0707b55e5d6139508ec54c9155514
April Hughes <dreydain@gmail.com>
2022-06-14 15:32:44 -0700

    lecture 81 close modal with escape key

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
-
 const openModal = function () {
 	modal.classList.remove('hidden');
 	overlay.classList.remove('hidden');
@@ -22,3 +20,10 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+	console.log(e.key);
+	if (e.key === 'Escape' && !modal.classList.contains('.hidden')) {
+		closeModal();
+	}
+});
